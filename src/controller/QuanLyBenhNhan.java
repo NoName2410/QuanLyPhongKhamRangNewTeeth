90dbe8835d147e71b4ffba62944db859c8e0664f
Tran Duc Luong <haychotoibietmatkhau@gmail.com>
2024-05-29 22:26:16 +0700

    Rename packages, update logic for login, sigup

---
+++ src/controller/QuanLyBenhNhan.java
@@ -0,0 +1,53 @@
+package controller;
+
+import model.BenhNhan;
+import views.MaBenhNhan;
+import java.util.ArrayList;
+
+public class QuanLyBenhNhan {
+
+    private ArrayList<BenhNhan> ds;
+    private int currentId;
+
+    public QuanLyBenhNhan() {
+        ds = new ArrayList<>();
+        currentId = MaBenhNhan.getCurrentID(); // Bắt đầu từ 1 hoặc giá trị mong muốn
+    }
+
+    private String generateMaBenhNhan() {
+        String maBenhNhan = "BN" + String.format("%03d", currentId);
+        currentId++;
+        return maBenhNhan;
+    }
+
+    private boolean isSoDienThoaiExists(String soDienThoai) {
+        for (BenhNhan bn : ds) {
+            if (bn.getSoDienThoai().equals(soDienThoai)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean themBenhNhan(BenhNhan bn) {
+        if (bn.isValid() && !isSoDienThoaiExists(bn.getSoDienThoai())) {
+            bn.setMaBenhNhan(generateMaBenhNhan());
+            ds.add(bn);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ArrayList<BenhNhan> getDanhSachBenhNhan() {
+        return ds;
+    }
+
+    public String getNextMaBenhNhan() {
+        return "BN" + String.format("%03d", currentId);
+    }
+
+    public void setCurrent(int currentId){
+        this.currentId = currentId;
+    }
+}
---
+++ src/views/MaBenhNhan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package views;
+
+/**
+ *
+ * @author PC
+ */
+public class MaBenhNhan {
+    private static int currentID;
+
+    public static int getCurrentID() {
+        return currentID;
+    }
+
+    public static void setCurrentID(int currentID) {
+        MaBenhNhan.currentID = currentID;
+    }
+
+}

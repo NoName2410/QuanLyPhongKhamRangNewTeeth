af7f8bedfe3f646814ed5a163064b41f6c3d6303
Tran Duc Luong <haychotoibietmatkhau@gmail.com>
2024-06-02 08:03:56 +0700

    try push to fix pull

---
+++ src/entity/HoaDon.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Acer
+ */
+public class HoaDon {
+    private int id;
+    private String name;
+    private String date;
+    private String service;
+    private double totalCost;
+
+    public HoaDon(int id, String name, String date, String service, double totalCost) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.service = service;
+        this.totalCost = totalCost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+}
